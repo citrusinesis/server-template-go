d4e6a70306a3f2168cd536e7a256921498d154ab
Jiho Song <jihojiho2003@gmail.com>
2025-01-07 04:37:19 +0300

    fix(middleware/log): turn on option to proper log

--- internal/core/middleware/logger.go
+++ internal/core/middleware/logger.go
@@ -10,19 +10,22 @@ import (
 func Logger(logger appLog.Logger) echo.MiddlewareFunc {
 	logger = logger.WithModule("request")
 	return middleware.RequestLoggerWithConfig(middleware.RequestLoggerConfig{
-		LogURI:    true,
-		LogStatus: true,
+		LogURI:     true,
+		LogStatus:  true,
+		LogMethod:  true,
+		LogLatency: true,
 		LogValuesFunc: func(c echo.Context, values middleware.RequestLoggerValues) error {
 			fields := appLog.Fields{
-				"URI":    values.URI,
-				"method": values.Method,
-				"status": values.Status,
+				"method":  values.Method,
+				"uri":     values.URI,
+				"status":  values.Status,
+				"latency": values.Latency,
 			}
 			if values.Error != nil {
 				fields["error"] = values.Error.Error()
-				logger.WithFields(fields).Error("request error")
+				logger.WithFields(fields).Error(values.Error.Error())
 			} else {
-				logger.WithFields(fields).Info("request ok")
+				logger.WithFields(fields).Info()
 			}
 			return nil
 		},
