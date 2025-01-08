c40650e700d3ab66f4a7315f2a29bd7476f8a7bf
Jiho Song <jihojiho2003@gmail.com>
2025-01-08 05:01:53 +0300

    feat: add helper function template in store package

---
+++ internal/core/store/helper.go
@@ -0,0 +1,10 @@
+package store
+
+/*
+ * Add your Helper Function here.
+ *
+ * func GetUserInfo(c echo.Context) *domain.User {
+ *     user := GetFromEcho[*domain.User](c)
+ * 	   return user
+ * }
+ */
